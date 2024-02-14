4044a727e7028fb7acaa1bde0525a5bc6ee0ffc9
Penguin <rati.bliadze10@gmail.com>
2024-02-14 23:29:59 +0400

    started implementing tribes

---
+++ Assets/_Assets/Scripts/Game/Tribe/Tribe.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RoundKnights
+{
+    public class Tribe : MonoBehaviour
+    {
+        [field: SerializeField] public ResourceBlock ResourceBlock { get; private set; }
+    }
+}
---
+++ Assets/_Assets/Scripts/Game/Tribe/TribeConfig.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace RoundKnights
+{
+    [CreateAssetMenu(menuName = "RoundKnights/Tribe/Tribe", fileName = "Tribe_Config")]
+    public class TribeConfig : ScriptableObject
+    {
+        [field: SerializeField] public string TribeNames { get; private set; } = "Tribe";
+    }
+}
