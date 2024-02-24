938b80f3d4b71191743e8714de357a3990d03c2e
Penguin <rati.bliadze10@gmail.com>
2024-02-25 03:07:53 +0400

    minor chages

--- Assets/_Assets/Scripts/Game/Tribe/Entities/Components/EntityMovement.cs
+++ Assets/_Assets/Scripts/Game/Tribe/Entities/Components/EntityMovement.cs
@@ -1,14 +1,81 @@
 using System;
+using System.Collections;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace RoundKnights
 {
     public class EntityMovement : MonoBehaviour
     {
+        const float DELTA_TIME = .02f;
+        const float DELTA_DISTANCE = .01f;
+        const float DELTA_SQR_DISTANCE = DELTA_DISTANCE * DELTA_DISTANCE;
+        
         [field: SerializeField, InlineEditor] public EntityMovementConfig Config { get; private set; }
         
+        public NavMeshAgent Agent { get; private set; }
         public float VelocityPercent { get; private set; }
+
+        public Vector3 TargetPos { get; set; }
+        public bool IsMoving { get; private set; }
+
+        bool IsNearEnoughToTarget => (TargetPos - transform.position).sqrMagnitude <= DELTA_SQR_DISTANCE;
+
+        Action m_OnComplete;
+        Action m_OnCancel;
+        
+        public void MoveToTarget(Vector3 targetPos, Action onComplete, Action onCancel)
+        {
+            TargetPos = targetPos;
+            m_OnComplete = onComplete;
+            if (IsNearEnoughToTarget)
+            {
+                m_OnComplete?.Invoke();
+                Agent.ResetPath();
+                if (m_OnCancel != null)
+                {
+                    m_OnCancel.Invoke();
+                    m_OnCancel = onCancel;
+                }
+                return;
+            }
+
+            Agent.SetDestination(TargetPos);
+            if (!IsMoving) StartCoroutine(MovementLoop());
+            else
+            {
+                m_OnCancel.Invoke();
+                m_OnCancel = onCancel;
+            }
+        }
+
+        IEnumerator MovementLoop()
+        {
+            IsMoving = true;
+
+            while (IsMoving)
+            {
+                yield return new WaitForSeconds(DELTA_TIME);
+            }
+            
+            IsMoving = false;
+        }
+
+        public void Terminate()
+        {
+            if (IsMoving)
+            {
+                IsMoving = false;
+                Agent.ResetPath();
+                m_OnComplete = null;
+                if (m_OnCancel != null)
+                {
+                    m_OnCancel.Invoke();
+                    m_OnCancel = null;
+                }
+            }
+        }
         
         #region Save&Load
 
@@ -25,6 +92,8 @@ namespace RoundKnights
             Trs = transform;
             Trs.position = saveFile.Trs.Position;
             Trs.eulerAngles = saveFile.Trs.Euler;
+
+            Agent = GetComponent<NavMeshAgent>();
         }
 
         public SaveFile GetSaveFile()
